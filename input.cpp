4d5cff64f2084fe9a6fac871099d4195cb4e97e1
RyanGD2025 <ryanbordez@gmail.com>
2025-11-18 12:29:15 -0300

    Add Input class with touch and key handling methods

---
+++ input.cpp
@@ -0,0 +1,19 @@
+// input.cpp
+#include "input.h"
+
+Input::Input() {
+    // Inicialização
+}
+
+Input::~Input() {
+    // Limpeza
+}
+
+void Input::processTouch(float x, float y, int action) {
+    // Tratamento do toque baseado na posição e ação (pressionar, soltar, mover)
+}
+
+bool Input::isKeyPressed(int keyCode) {
+    // Retorna estado da tecla
+    return false;
+}
