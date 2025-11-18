065de10fd571fc49e7858b513a6d5052000c0fea
RyanGD2025 <ryanbordez@gmail.com>
2025-11-18 12:46:48 -0300

    Implement GameLogic class with basic methods

---
+++ game_logic.cpp
@@ -0,0 +1,23 @@
+// game_logic.cpp
+#include "game_logic.h"
+
+GameLogic::GameLogic() {
+    // Inicialização
+}
+
+GameLogic::~GameLogic() {
+    // Limpeza
+}
+
+void GameLogic::reset() {
+    // Reseta estado do jogo
+}
+
+void GameLogic::update(float deltaTime) {
+    // Atualiza estado do jogo baseado no deltaTime
+}
+
+bool GameLogic::isGameOver() {
+    // Verifica condição de fim de jogo
+    return false;
+}
