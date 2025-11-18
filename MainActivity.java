49068241b468eddc66be53f10d866ddcf9cd23c5
RyanGD2025 <ryanbordez@gmail.com>
2025-11-18 11:07:58 -0300

    Implement WebView to load home.html in MainActivity

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.cubegames.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
+        // Carrega a página home.html que deve estar em assets
+        webView.loadUrl("file:///android_asset/home.html");
+    }
+}
