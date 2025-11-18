ff977b93ceae8ce075e346fd1c418488e6c879cc
RyanGD2025 <ryanbordez@gmail.com>
2025-11-18 12:30:57 -0300

    Implement basic Renderer class methods

---
+++ renderer.cpp
@@ -0,0 +1,22 @@
+// renderer.cpp
+#include "renderer.h"
+
+Renderer::Renderer() {
+    // Configuração inicial de renderização
+}
+
+Renderer::~Renderer() {
+    // Libera recursos gráficos
+}
+
+void Renderer::clearScreen() {
+    // Limpa a tela para o próximo frame
+}
+
+void Renderer::drawSprite(int spriteId, float x, float y) {
+    // Desenha o sprite na posição
+}
+
+void Renderer::present() {
+    // Apresenta o frame
+}
